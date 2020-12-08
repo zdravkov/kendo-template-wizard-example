a3406304ebc8d73d579e17409391f848d86e9053
zdravkov <zdravkov.pl@gmail.com>
2020-12-08 11:25:13 +0200

    chore: add vue 2 version

---
+++ src/components/MenuNavContainer.vue
@@ -0,0 +1,31 @@
+﻿<template>
+    <Menu>
+      <li>
+        <router-link class="k-link k-menu-link" to="/">Home</router-link>
+      </li>
+        <li>
+        <router-link class="k-link k-menu-link" to="/Blank1">Blank1</router-link>
+        </li>
+        <li>
+        <router-link class="k-link k-menu-link" to="/Grid2">Grid2</router-link>
+        </li>
+        <li>
+        <router-link class="k-link k-menu-link" to="/Form1">Form1</router-link>
+        </li>
+        <li>
+        <router-link class="k-link k-menu-link" to="/Grid1">Grid1</router-link>
+        </li>
+        <li>
+        <router-link class="k-link k-menu-link" to="/Chart1">Chart1</router-link>
+        </li>
+    </Menu>
+</template>
+<script>
+import '@progress/kendo-ui/js/kendo.menu';
+import { Menu } from "@progress/kendo-layout-vue-wrapper";
+export default {
+    components: {
+       Menu
+    }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+﻿import Vue from "vue";
+import Router from "vue-router";
+import Home from "../components/Home";
+import Blank1 from "../components/Blank1";
+import Grid2 from "../components/Grid2";
+import Form1 from "../components/Form1";
+import Grid1 from "../components/Grid1";
+import Chart1 from "../components/Chart1";
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    }
+    ,{
+      path: "/Blank1",
+      name: "Blank1",
+      component: Blank1
+    }
+    ,{
+      path: "/Grid2",
+      name: "Grid2",
+      component: Grid2
+    }
+    ,{
+      path: "/Form1",
+      name: "Form1",
+      component: Form1
+    }
+    ,{
+      path: "/Grid1",
+      name: "Grid1",
+      component: Grid1
+    }
+    ,{
+      path: "/Chart1",
+      name: "Chart1",
+      component: Chart1
+    }
+  ]
+});
